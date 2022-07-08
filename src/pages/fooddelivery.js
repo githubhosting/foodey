3d433ffeb03e1fe605650e121b05f74991d64c0e
myselfshravan <shravanrevanna@gmail.com>
2022-07-08 18:14:59 +0530

    second try commit

--- src/pages/fooddelivery.js
+++ src/pages/fooddelivery.js
@@ -1,28 +1,28 @@
-import React, { Fragment } from 'react';
-import Head from 'next/head';
-import { ThemeProvider } from 'styled-components';
-import { DrawerProvider } from 'common/contexts/DrawerContext';
-import Sticky from 'react-stickynode';
-import { foodDeliveryTheme } from 'common/theme/foodDelivery';
-import ResetCSS from 'common/assets/css/style';
+import React, { Fragment } from "react";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import { DrawerProvider } from "common/contexts/DrawerContext";
+import Sticky from "react-stickynode";
+import { foodDeliveryTheme } from "common/theme/foodDelivery";
+import ResetCSS from "common/assets/css/style";
 import {
   GlobalStyle,
   ContentWrapper,
-} from '../containers/FoodDelivery/foodDelivery.style';
+} from "../containers/FoodDelivery/foodDelivery.style";
 
-import Navbar from '../containers/FoodDelivery/Navbar';
-import Testimonials from '../containers/FoodDelivery/Testimonials';
-import Banner from '../containers/FoodDelivery/Banner';
-import AvailableRestaurants from '../containers/FoodDelivery/AvailableRestaurants';
-import PopularCategory from '../containers/FoodDelivery/PopularCategory';
-import HowWorks from '../containers/FoodDelivery/HowWorks';
-import ProductDelivery from '../containers/FoodDelivery/ProductDelivery';
-import QualityFeatures from '../containers/FoodDelivery/QualityFeatures';
-import Clients from '../containers/FoodDelivery/Clients';
-import DownloadApp from '../containers/FoodDelivery/DownloadApp';
-import Gallery from '../containers/FoodDelivery/Gallery';
-import CallToAction from '../containers/FoodDelivery/CallToAction';
-import Footer from '../containers/FoodDelivery/Footer';
+import Navbar from "../containers/FoodDelivery/Navbar";
+import Testimonials from "../containers/FoodDelivery/Testimonials";
+import Banner from "../containers/FoodDelivery/Banner";
+import AvailableRestaurants from "../containers/FoodDelivery/AvailableRestaurants";
+import PopularCategory from "../containers/FoodDelivery/PopularCategory";
+import HowWorks from "../containers/FoodDelivery/HowWorks";
+import ProductDelivery from "../containers/FoodDelivery/ProductDelivery";
+import QualityFeatures from "../containers/FoodDelivery/QualityFeatures";
+import Clients from "../containers/FoodDelivery/Clients";
+import DownloadApp from "../containers/FoodDelivery/DownloadApp";
+import Gallery from "../containers/FoodDelivery/Gallery";
+import CallToAction from "../containers/FoodDelivery/CallToAction";
+import Footer from "../containers/FoodDelivery/Footer";
 
 const FoodDelivery = () => {
   return (
