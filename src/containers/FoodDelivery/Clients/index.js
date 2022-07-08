380f417c0de5241235c7dd4574be18d34ddcbfe2
myselfshravan <shravanrevanna@gmail.com>
2022-07-08 19:26:29 +0530

    final cehck commit

--- src/containers/FoodDelivery/Clients/index.js
+++ src/containers/FoodDelivery/Clients/index.js
@@ -5,24 +5,24 @@ import NextImage from 'common/components/NextImage';
 import Box from 'common/components/Box';
 import ClientsWrapper from './clients.style';
 // import { CLIENTS_DATA } from 'common/data/FoodDelivery';
-const Clients = () => {
-  const { title, images } = CLIENTS_DATA;
-  return (
-    <ClientsWrapper>
-      <Container>
-        <Heading as="h2" content={title} />
-        <Box className="clientImages">
-          {images.map(({ path }, index) => (
-            <NextImage
-              src={path}
-              key={`clients-image-key-${index}`}
-              alt="client image"
-            />
-          ))}
-        </Box>
-      </Container>
-    </ClientsWrapper>
-  );
-};
+// const Clients = () => {
+//   const { title, images } = CLIENTS_DATA;
+//   return (
+//     <ClientsWrapper>
+//       <Container>
+//         <Heading as="h2" content={title} />
+//         <Box className="clientImages">
+//           {images.map(({ path }, index) => (
+//             <NextImage
+//               src={path}
+//               key={`clients-image-key-${index}`}
+//               alt="client image"
+//             />
+//           ))}
+//         </Box>
+//       </Container>
+//     </ClientsWrapper>
+//   );
+// };
 
-export default Clients;
+// export default Clients;
